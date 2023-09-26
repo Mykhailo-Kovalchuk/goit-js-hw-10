458c44e13926cb31c710e764f1c0769e7d2ed8b0
Mykhailo-Kovalchuk <contact.kovalchuk@gmail.com>
2023-09-26 09:53:05 +0200

    goit-js-hw-10-commit nr 4

--- src/cat-api.js
+++ src/cat-api.js
@@ -12,49 +12,49 @@ const BASE_URL = "https://api.thecatapi.com/v1/images/search"
 import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = API_KEY;
 
-// export async function fetchBreeds() {
-//     try {
-//         const response = await axios.get(API_URL);
+export async function fetchBreeds() {
+    try {
+        const response = await axios.get(API_URL);
 
-//         return response.data;
-//     } catch (error) {
-//         throw error;
-//     }
-// }
-
-export function fetchBreeds() {
-const errors = document.querySelector('.error')
-   return axios.get(API_URL)
-.then(response => {
-    return response.data.map((breed) => ({
-        id: breed.id,
-        name: breed.name
-    }));
-})
-.catch(() => { 
-    Notify.failure(errors.textContent)})
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
 }
 
-
-// export async function fetchCatByBreed(breedId) {
-//     try {
-//         const response = await axios.get(
-//             `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
-//         );
-//         return response.data[0];
-//     } catch (error) {
-// throw error;
-//     }
+// export function fetchBreeds() {
+// const errors = document.querySelector('.error')
+//    return axios.get(API_URL)
+// .then(response => {
+//     return response.data.map((breed) => ({
+//         id: breed.id,
+//         name: breed.name
+//     }));
+// })
+// .catch(() => { 
+//     Notify.failure(errors.textContent)})
 // }
 
 
-export function fetchCatByBreed(breedId) {
-   return axios.get(`${BASE_URL}?breed_ids=${breedId}`)
-.then(response => {
-    return response.data[0]
-})
-.catch(() => {
-    Notify.failure('Ой лишенько! Щось пішло не так:(');
-console.log("erorrrrrrr");
-});
+export async function fetchCatByBreed(breedId) {
+    try {
+        const response = await axios.get(
+            `${BASE_URL}?breed_ids=${breedId}`
+        );
+        return response.data[0];
+    } catch (error) {
+throw error;
+    }
 }
+
+
+// export function fetchCatByBreed(breedId) {
+//    return axios.get(`${BASE_URL}?breed_ids=${breedId}`)
+// .then(response => {
+//     return response.data[0]
+// })
+// .catch(() => {
+//     Notify.failure('Ой лишенько! Щось пішло не так:(');
+// console.log("erorrrrrrr");
+// });
+// }
